6c92498731e255adce974b9e53dd3fa40cb28d6d
Mnk242 <rsharma@traversia.net>
2024-09-03 15:51:47 +0530

    feat: add banner group

--- src/app/banner-group/banner-group.component.ts
+++ src/app/banner-group/banner-group.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+
+interface Banner {
+  _id: number;
+  title:string;
+  subtitle: string;
+  image: string;
+}
 @Component({
   selector: 'app-banner-group',
   templateUrl: './banner-group.component.html',
   styleUrls: ['./banner-group.component.css']
 })
 export class BannerGroupComponent {
+  banners = [
+    {
+      _id: 1,
+      title: "Inbuilt Expense Management Tool",
+      subtitle: "Single platform for filing expenses and ensuring easy reconciliation",
+      image: "home-page-banner.webp"
+    },
+    {
+      _id: 2,
+      title: "Save on Company Travel Expenses",
+      subtitle: "With special corporate rates on flights & hotels, save your travel budget by paying less for more features",
+      image: "home-page-banner.webp"
+    },
+    {
+      _id: 3,
+      title: "Inbuilt Expense Management Tool",
+      subtitle: "Single platform for filing expenses and ensuring easy reconciliation",
+      image: "home-page-banner.webp"
+    },
+    {
+      _id: 4,
+      title: "Save on Company Travel Expenses",
+      subtitle: "With special corporate rates on flights & hotels, save your travel budget by paying less for more features",
+      image: "home-page-banner.webp"
+    },
+  ];
+  animate= false;
+  selectedBanner = this.banners[0];
 
+  selectBanner(banner: Banner){
+    this.animate = true;
+    setTimeout(()=>{
+      this.animate = false;
+    }, 300)
+    this.selectedBanner = banner;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-showMenu = true;
+showMenu = false;
 toggleMenu(){
   this.showMenu =!this.showMenu;
 }
